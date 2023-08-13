292b9fca54229007816c6e87534abbae8c26bdea
andybaumgar <andybaumgar@gmail.com>
2023-08-13 02:08:51 -0400

    Add ac fan

--- apartment_controller/ac_controller.py
+++ apartment_controller/ac_controller.py
@@ -1,39 +1,49 @@
-from kasa import SmartPlug
-from dotenv import load_dotenv
 import os
+from time import sleep
+
+from dotenv import load_dotenv
+from kasa import SmartPlug
+
+from apartment_controller import config
 from apartment_controller.utils.async_utils import async_to_sync
 from apartment_controller.utils.time_utils import sleep_hours
-from time import sleep
-from apartment_controller import config
 
 load_dotenv()
 
 
 @async_to_sync
-async def turn_off_ac(smart_plug):
-    await smart_plug.turn_off()
+async def turn_off_ac(smart_plugs):
+    for smart_plug in smart_plugs:
+        await smart_plug.turn_off()
     print("AC turned off")
 
 
 @async_to_sync
-async def turn_on_ac(smart_plug):
-    await smart_plug.turn_on()
+async def turn_on_ac(smart_plugs):
+    for smart_plug in smart_plugs:
+        await smart_plug.turn_on()
     print("AC turned on")
 
 
 def run():
     print("Running AC controller")
-    smart_plug = SmartPlug(config.ac_plug_ip)
+    ac_smart_plug = SmartPlug(config.ac_plug_ip)
+    fan_smart_plug = SmartPlug(config.ac_fan_ip)
+    smart_plugs = [ac_smart_plug, fan_smart_plug]
     print("Connected to smart plug")
 
     while True:
-        turn_off_ac(smart_plug)
+        turn_off_ac(smart_plugs)
         sleep_hours(0.75)
         print("starting sleep")
-        turn_on_ac(smart_plug)
+        turn_on_ac(smart_plugs)
         print("starting sleep")
         sleep_hours(4)
 
 
 if __name__ == "__main__":
-    run()
+    ac_smart_plug = SmartPlug(config.ac_plug_ip)
+    fan_smart_plug = SmartPlug(config.ac_fan_ip)
+    smart_plugs = [ac_smart_plug, fan_smart_plug]
+    # run()
+    turn_off_ac([ac_smart_plug, fan_smart_plug])
--- apartment_controller/config.py
+++ apartment_controller/config.py
@@ -1,6 +1,7 @@
 latitude = 40.7128  # New York City
 longitude = -74.0060  # New York City
 ac_plug_ip = "192.168.0.128"
+ac_fan_ip = "192.168.0.254"
 light_plug_ips = ["192.168.0.205", "192.168.0.136", "192.168.0.232"]
 bed_time_hour = 2
 wake_up_hour = 8
